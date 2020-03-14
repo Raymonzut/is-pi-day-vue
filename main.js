c3d289e29809f5a2b4d0e6e8aae22c74cb61ec3d
Raymonzut <raymonzutekouw@gmail.com>
2020-03-14 19:23:54 +0100

    Display if it is pi day

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+const now = new Date();
+
+const is_right_day = now.getDate() === 14;
+const is_right_month = (now.getMonth() + 1) === 3;
+
+const is_pi_day = is_right_day && is_right_month;
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: "Hello World!"
+    message: "It is" + (is_pi_day ? " " : " not ") + "pi day"
   },
 })
