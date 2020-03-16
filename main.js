2141d4ad94b13bc93fc0b41cf6e6412be0deb2a2
Raymonzut <raymonzutekouw@gmail.com>
2020-03-16 22:57:07 +0100

    Stream in the digits of pi

--- main.js
+++ main.js
@@ -5,10 +5,18 @@ const is_right_month = (now.getMonth() + 1) === 3;
 
 const is_pi_day = is_right_day && is_right_month;
 
+pi = get_pi(10000n);
+
 var app = new Vue({
   el: '#app',
   data: {
     message: "It is" + (is_pi_day ? " " : " not ") + "pi day",
-    pi: get_pi(),
+    pi: "",
   },
 })
+
+index = 0
+setInterval(() => {
+  index++;
+  app.pi = pi.slice(0, index);
+});
